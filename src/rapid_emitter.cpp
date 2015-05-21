6d8e5efb09a660cee09e4ae64184b0429140440c
Jonathan Meyer <jonathan.meyer@swri.org>
2015-05-21 18:05:22 -0500

    Fleshed out

--- src/rapid_emitter.cpp
+++ src/rapid_emitter.cpp
@@ -9,14 +9,41 @@ bool rapid_emitter::emitRapidFile(const std::string& filename,
                                  const ProcessParams& params)
 {
   // Write header
+  std::cout << "MODULE mGodel_Blend\n\n";
   // Emit all of the joint points
+  for (std::size_t i = 0; i < points.size(); ++i)
+  {
+    emitJointPosition(std::cout, points[i], i);
+  }
   // Write beginning of procedure
+  std::cout << "\nPROC TestProc()";
   // For 0 to lengthFreeMotion, emit free moves
+  for (std::size_t i = 0; i < lengthFreeMotion; ++i)
+  {
+    emitFreeMotion(std::cout, params, i);
+  }
   // for lengthFreeMotion to end of points, emit grind moves
+  for (std::size_t i = lengthFreeMotion; i < points.size(); ++i)
+  {
+    emitGrindMotion(std::cout, params, i);
+  }
+  std::cout << "EndProc";
   // write any footers including main procedure calling the above
-  return false;
+  std::cout << "ENDMODULE";
+  return true;
 }
 
+bool rapid_emitter::emitGrindMotion(std::ostream& os, const ProcessParams& params, size_t n)
+{
+  os << "GrindL CalcRobT(jTarget_" << n << ",tool0), v100, z40, tool0;\n";
+  return os.good();
+}
+
+bool rapid_emitter::emitFreeMotion(std::ostream& os, const ProcessParams& params, size_t n)
+{
+  os << "MoveL CalcRobT(jTarget_" << n << ",tool0), v100, z40, tool0;\n";
+  return os.good();
+}
 bool rapid_emitter::emitJointPosition(std::ostream& os, const TrajectoryPt& pt, size_t n)
 {
   os << "TASK PERS jointtarget jTarget_" << n << ":=[[";
